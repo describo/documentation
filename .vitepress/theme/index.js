5cd8eb5d34a4bee4e44d90ee0633b24acbed1fd1
Marco La Rosa <m@lr.id.au>
2024-03-06 15:02:05 +1100

    install and wire up medium-zoom for all images

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -1,4 +1,26 @@
 import "./tailwind.postcss";
 
 import DefaultTheme from "vitepress/dist/client/theme-default/index.js";
-export default { ...DefaultTheme };
+import { onMounted, watch, nextTick } from "vue";
+import mediumZoom from "medium-zoom";
+import { useRoute } from "vitepress";
+
+import "./index.css";
+
+export default {
+    ...DefaultTheme,
+    setup() {
+        const route = useRoute();
+        const initZoom = () => {
+            if (!route.path.match(/tour.html/))
+                mediumZoom(".main img", { background: "var(--vp-c-bg)" });
+        };
+        onMounted(() => {
+            initZoom();
+        });
+        watch(
+            () => route.path,
+            () => nextTick(() => initZoom())
+        );
+    },
+};
